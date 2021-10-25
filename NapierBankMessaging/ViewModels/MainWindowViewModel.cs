f6d248d687f34d658e43b48e560bd00f8cb166d1
40478448 <40478448@ME0MC11-200916>
2021-10-25 11:33:41 +0100

    Added: Data Binding for Text and Buttons

--- NapierBankMessaging/ViewModels/MainWindowViewModel.cs
+++ NapierBankMessaging/ViewModels/MainWindowViewModel.cs
@@ -1,16 +1,32 @@
 ﻿using NapierBankMessaging.ViewModels;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Input;
+using NapierBankMessaging.Commands;
+using System.Windows;
 
 namespace NapierBankMessaging.Views
 {
     class MainWindowViewModel : BaseViewModel
     {
 
+        public string HeaderText { get; private set; }
+        public string ImportBtnText { get; private set; }
 
+        public ICommand ImportJSONDataCommand { get; private set; }
 
+        public MainWindowViewModel()
+        {
+            HeaderText = "Napier Bank Messaging";
+
+            ImportBtnText = "Import JSON File";
+
+            ImportJSONDataCommand = new RelayCommand(ImportBtnClick);
+
+        }
+
+        private void ImportBtnClick()
+        {
+            MessageBox.Show("Functionality Working");
+        }
     }
 }
