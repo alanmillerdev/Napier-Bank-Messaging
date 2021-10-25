9e9846e9c00baf9249459691bed5327c2dce915f
40478448 <40478448@ME0MC11-200916>
2021-10-25 11:13:24 +0100

    Add: RelayCommand, BaseViewModel, MainWindowViewModel

---
+++ NapierBankMessaging/Commands/RelayCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace NapierBankMessaging.Commands
+{
+    public class RelayCommand : ICommand
+    {
+
+        private Action _action;
+
+        public event EventHandler CanExecuteChanged = (sender, e) => { };
+
+        public RelayCommand(Action action)
+        {
+            _action = action;
+        }
+
+        public bool CanExecute(Object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+    }
+}
---
+++ NapierBankMessaging/ViewModels/BaseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace NapierBankMessaging.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnChanged(string propertyChanged)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyChanged));
+        }
+
+    }
+}
